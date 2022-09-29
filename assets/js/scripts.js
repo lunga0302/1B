2d978cbe086deea2f22dc26d2655838f33f8b11a
0302lunga <lungathe3rd@gmail.com>
2022-09-29 14:05:37 +0200

    aSSIGNMENT 1D

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+/*!
+* Start Bootstrap - Bare v5.0.8 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+var data = [1 2 3 4 5]
+function load data
